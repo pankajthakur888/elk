8f12f5a4fadfc646c1366e4b291b8d0aa899f925
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2021-01-20 17:24:19 +0530

    Update install.elk.sh

--- install.elk.sh
+++ install.elk.sh
@@ -8,9 +8,10 @@
 	sudo apt install net-tools -y
   
 #Install Dependency
-  sudo apt install wget git curl
+  sudo apt install wget git curl openjdk-11-jre-headless -y
 
 #ELK 
+
 ##- Elastic Search
   mkdir -p /opt/elk
   cd /opt/elk/
@@ -18,6 +19,20 @@
   tar -xzf elasticsearch-7.10.2-linux-x86_64.tar.gz
   cd elasticsearch-7.10.2/ 
   ./bin/elasticsearch
-
-  
-
+  sudo ln -s "${PWD}/bin/elasticsearch" /usr/local/bin/
+ 
+ ##- Kibana
+ 	cd /opt/elk/
+	curl -O https://artifacts.elastic.co/downloads/kibana/kibana-7.10.2-linux-x86_64.tar.gz
+	tar -xzf kibana-7.10.2-linux-x86_64.tar.gz
+	cd kibana-7.10.2-linux-x86_64/
+	./bin/kibana
+	sudo ln -s "${PWD}/bin/kibana" /usr/local/bin/
+	
+##- Logstash
+	cd /opt/elk/
+	wget https://artifacts.elastic.co/downloads/logstash/logstash-7.10.2-linux-x86_64.tar.gz
+	tar -xzf logstash-7.10.2-linux-x86_64.tar.gz
+	cd logstash-7.10.2-linux-x86_64/
+	./bin/logstash -f logstash.conf
+	sudo ln -s "${PWD}/bin/logstash" /usr/local/bin/
